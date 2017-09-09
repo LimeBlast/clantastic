780abefe11005519736b413fd0262b9a70b064d7
Daniel Hollands <daniel@limeblast.co.uk>
2017-09-09 19:11:31 +0100

    Removed the push button code

--- family-box.ino
+++ family-box.ino
@@ -1,12 +1,3 @@
-// Instructables Internet of Things Class sample code
-// Circuit Triggers Internet Action
-// A button press is detected and stored in a feed
-// An LED is used as confirmation feedback
-//
-// Modified by Becky Stern 2017
-// based on the Adafruit IO Digital Input Example
-// Tutorial Link: https://learn.adafruit.com/adafruit-io-basics-digital-input
-//
 // Adafruit invests time and resources providing this open source code.
 // Please support Adafruit and open source hardware by purchasing
 // products from Adafruit!
@@ -20,8 +11,7 @@
 /************************** Configuration ***********************************/
 
 // edit the config.h tab and enter your Adafruit IO credentials
-// and any additional configuration needed for WiFi, cellular,
-// or ethernet clients.
+// and any additional configuration needed for WiFi client.
 #include "config.h"
 
 #include "AdafruitIO_WiFi.h"
@@ -32,10 +22,6 @@ AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 #include <AdafruitIO.h>
 #include <Adafruit_MQTT.h>
 #include <ArduinoHttpClient.h>
-
-#define BUTTON_PIN 4
-#define LED_PIN 13
-
 #include <Adafruit_NeoPixel.h>
 
 #define PIXELS_PIN 15
@@ -44,24 +30,15 @@ AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIXELS_PIN, NEO_GRB + NEO_KHZ800);
 
-// button state
-int current = 0;
-int last = 0;
-
 int red = 0;
 int green = 0;
 int blue = 0;
+char hex[8] = {0};
 
-// set up the 'command' feed
-AdafruitIO_Feed *command = io.feed("command");
+// set up the 'family-box' group
 AdafruitIO_Group *group = io.group("family-box");
 
 void setup() {
-
-  // set button pin as an input
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  pinMode(LED_PIN, OUTPUT);
-
   strip.setBrightness(BRIGHTNESS);
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
@@ -73,14 +50,13 @@ void setup() {
   Serial.print("Connecting to Adafruit IO");
   io.connect();
   
-  // set up a message handler for the 'command' feed.
+  // set up a message handlers for the group feed.
   // the handleMessage function (defined below)
   // will be called whenever a message is
   // received from adafruit io.
-  command->onMessage(handleMessage);
-  group->onMessage("daniel", handleColour);
-  group->onMessage("jessica", handleColour);
-  group->onMessage("emily", handleColour);
+  group->onMessage("daniel", handleMessage);
+  group->onMessage("jessica", handleMessage);
+  group->onMessage("emily", handleMessage);
 
   // wait for a connection
   while(io.status() < AIO_CONNECTED) {
@@ -95,54 +71,17 @@ void setup() {
 }
 
 void loop() {
-
   // io.run(); is required for all sketches.
   // it should always be present at the top of your loop
   // function. it keeps the client connected to
   // io.adafruit.com, and processes any incoming data.
   io.run();
-
-  // grab the current state of the button.
-  // we have to flip the logic because we are
-  // using INPUT_PULLUP.
-  if(digitalRead(BUTTON_PIN) == LOW)
-    current = 1;
-  else
-    current = 0;
-
-  // return if the value hasn't changed
-  if(current == last)
-    return;
-
-  // save the current state to the 'command' feed on adafruit io
-  Serial.print("sending button -> ");
-  Serial.println(current);
-  command->save(current);
-
-  // store last button state
-  last = current;
 }
 
 // this function is called whenever a 'command' message
 // is received from Adafruit IO. it was attached to
 // the command feed in the setup() function above.
 void handleMessage(AdafruitIO_Data *data) {
-
-  int command = data->toInt();
-
-  if (command == 1){ //light up the LED
-    Serial.print("received <- ");
-    Serial.println(command);
-    digitalWrite(LED_PIN, HIGH);
-    delay(500);
-    digitalWrite(LED_PIN, LOW);
-  } else {
-    Serial.print("received <- ");
-    Serial.println(command);
-  }
-}
-
-void handleColour(AdafruitIO_Data *data) {
   if (String(data->feedName()).equalsIgnoreCase("daniel")) {
     green = data->toInt();
   } else if (String(data->feedName()).equalsIgnoreCase("jessica")) {
@@ -161,4 +100,10 @@ void handleColour(AdafruitIO_Data *data) {
     strip.setPixelColor(i, strip.Color(red,green,blue));
   }
   strip.show(); //always remember to call strip.show() to display changes
+
+  sprintf(hex,"#%02X%02X%02X",red,green,blue);
+  Serial.println(hex); //Print the string.
+
+//  group->set("hex", hex);
+//  group->save();
 }
